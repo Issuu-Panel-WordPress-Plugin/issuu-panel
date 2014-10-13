93ab95ca50bbc5efb3fa3d4f59a60d4e88ca5574
Pedro Marcelo <pedromarcelodesaalves@gmail.com>
2014-10-13 00:04:29 -0300

    Criando quicksort para ordenação de documentos

--- includes/functions.php
+++ includes/functions.php
@@ -1,2 +1,43 @@
 <?php
 
+function issuu_painel_quick_sort($array, $order = 'asc')
+{
+	$length = count($array);
+	
+	if ($length <= 1)
+	{
+		return $array;
+	}
+	else
+	{
+		$pivot = $array[0];
+		$left = $right = array();
+		$c = count($array);
+		for ($i = 1; $i < $c; $i++) {
+			if ($order == 'asc')
+			{
+				if ($array[$i]['pubTime'] < $pivot['pubTime'])
+				{
+					$left[] = $array[$i];
+				}
+				else
+				{
+					$right[] = $array[$i];
+				}
+			}
+			else
+			{
+				if ($array[$i]['pubTime'] > $pivot['pubTime'])
+				{
+					$left[] = $array[$i];
+				}
+				else
+				{
+					$right[] = $array[$i];
+				}
+			}
+		}
+		
+		return array_merge(issuu_painel_quick_sort($left), array($pivot), issuu_painel_quick_sort($right));
+	}
+}
