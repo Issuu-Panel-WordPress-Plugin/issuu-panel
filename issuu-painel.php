9fb129ff9db7f701cde767fe025a46702b218839
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2014-10-15 18:01:18 -0300

    Inserindo request de atualização das chaves na action admin_menu

--- issuu-painel.php
+++ issuu-painel.php
@@ -28,22 +28,18 @@ $api_key = get_option(ISSUU_PAINEL_PREFIX . 'api_key');
 $api_secret = get_option(ISSUU_PAINEL_PREFIX . 'api_secret');
 
 require(ISSUU_PAINEL_DIR . 'menu/principal/config.php');
+require(ISSUU_PAINEL_DIR . 'issuuservice/issuu-lib.php');
 
-if ((!is_null($api_key) && strlen($api_key) > 0) && (!is_null($api_secret) && strlen($api_secret) > 0))
-{
-	require(ISSUU_PAINEL_DIR . 'issuuservice/issuu-lib.php');
+$includes = glob(ISSUU_PAINEL_DIR . 'includes/*.php');
 
-	$includes = glob(ISSUU_PAINEL_DIR . 'includes/*.php');
-
-	foreach ($includes as $include) {
-		require($include);
-	}
-
-	include(ISSUU_PAINEL_DIR . 'menu/documento/config.php');
-	include(ISSUU_PAINEL_DIR . 'menu/pasta/config.php');
-	include(ISSUU_PAINEL_DIR . 'shortcode/document-list.php');
-	include(ISSUU_PAINEL_DIR . 'shortcode/folder-list.php');
+foreach ($includes as $include) {
+	require($include);
 }
+
+include(ISSUU_PAINEL_DIR . 'menu/documento/config.php');
+include(ISSUU_PAINEL_DIR . 'menu/pasta/config.php');
+include(ISSUU_PAINEL_DIR . 'shortcode/document-list.php');
+include(ISSUU_PAINEL_DIR . 'shortcode/folder-list.php');
 
 add_action('wp_enqueue_scripts', 'issuu_painel_wp_enqueue_scripts');
 
@@ -74,7 +70,40 @@ function issuu_painel_admin_enqueue_scripts()
 	wp_enqueue_style('folder-list', ISSUU_PAINEL_URL . 'css/folder-list.css', array('dashicons'), null, 'screen, print');
 	wp_enqueue_script('json2');
 	wp_enqueue_script('jquery');
-	wp_enqueue_script('issuu-painel-js', ISSUU_PAINEL_URL . 'js/js.php');
+
+	if (isset($_GET['page']) && $_GET['page'] == 'issuu-document-admin')
+	{
+		if (isset($_GET['upload']))
+		{
+			wp_enqueue_script(
+				'issuu-painel-document-upload-js',
+				ISSUU_PAINEL_URL . 'js/document-upload.min.js',
+				array('jquery'),
+				null,
+				true
+			);
+		}
+		else if (isset($_GET['update']))
+		{
+			wp_enqueue_script(
+				'issuu-painel-document-update-js',
+				ISSUU_PAINEL_URL . 'js/document-update.min.js',
+				array('jquery'),
+				null,
+				true
+			);
+		}
+		else if (isset($_GET['url_upload']))
+		{
+			wp_enqueue_script(
+				'issuu-painel-document-url-upload-js',
+				ISSUU_PAINEL_URL . 'js/document-url-upload.min.js',
+				array('jquery'),
+				null,
+				true
+			);
+		}
+	}
 }
 
 add_action('admin_menu', 'ip_menu_admin');
@@ -84,6 +113,15 @@ function ip_menu_admin()
 	global $api_key, $api_secret;
 
 	do_action(ISSUU_PAINEL_PREFIX . 'menu_page');
+
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && (isset($_GET['page']) && $_GET['page'] == 'issuu-painel-admin'))
+	{
+		update_option(ISSUU_PAINEL_PREFIX . 'api_key', trim($_POST['api_key']));
+		update_option(ISSUU_PAINEL_PREFIX . 'api_secret',trim($_POST['api_secret']));
+
+		$api_key = trim($_POST['api_key']);
+		$api_secret = trim($_POST['api_secret']);
+	}
 
 	if ((!is_null($api_key) && strlen($api_key) > 0) && (!is_null($api_secret) && strlen($api_secret) > 0))
 	{
