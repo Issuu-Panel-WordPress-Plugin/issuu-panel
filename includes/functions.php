9ba4232fc3baa6df9c537030802458233b24f81f
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2014-10-16 18:04:55 -0300

    Inserindo funções para adicionar botão no editor

--- includes/functions.php
+++ includes/functions.php
@@ -41,3 +41,46 @@ function issuu_painel_quick_sort($array, $order = 'asc')
 		return array_merge(issuu_painel_quick_sort($left), array($pivot), issuu_painel_quick_sort($right));
 	}
 }
+
+function get_issuu_message($text)
+{
+	return __($text, ISSUU_PAINEL_DOMAIN_LANG);
+}
+
+function the_issuu_message($text)
+{
+	_e($text, ISSUU_PAINEL_DOMAIN_LANG);
+}
+
+function add_youtube_button()
+{
+	if (!current_user_can('edit_posts') && !current_user_can('edit_pages'))
+		return;
+	if (get_user_option('rich_editing') == 'true')
+	{
+		add_filter('mce_external_plugins', 'add_issuu_painel_tinymce_plugin');
+		add_filter('mce_buttons', 'register_issuu_painel_button');
+	}
+}
+
+add_action('init', 'add_youtube_button');
+
+function register_issuu_painel_button($buttons)
+{
+	array_push($buttons, "|", "issuupainel");
+	return $buttons;
+}
+
+function add_issuu_painel_tinymce_plugin($plugin_array)
+{
+	$plugin_array['issuupainel'] = ISSUU_PAINEL_URL . 'js/tinymce-button.js';
+	return $plugin_array;
+}
+
+function issuu_painel_refresh_mce($ver)
+{
+	$ver += 3;
+	return $ver;
+}
+
+add_filter('tiny_mce_version', 'issuu_painel_refresh_mce');
