09b7009d1d23acc5d664864116700bf639dc79f8
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2014-10-20 18:02:13 -0300

    Adicionando novas configurações, novo arquivo Javacript nas páginas do site e atualizando request das configurações do plugin

--- issuu-painel.php
+++ issuu-painel.php
@@ -14,6 +14,13 @@ define('ISSUU_PAINEL_URL', plugin_dir_url(__FILE__));
 define('ISSUU_PAINEL_PREFIX', 'issuu_painel_');
 define('ISSUU_PAINEL_DOMAIN_LANG', 'issuu-painel-domain-lang');
 
+$issuu_shortcode_index = 0;
+$issuu_painel_capabilities = array(
+	'Administrator' => 'manage_options',
+	'Editor' => 'edit_private_pages',
+	'Author' => 'upload_files'
+);
+
 add_action('plugins_loaded', 'issuu_painel_textdomain');
 
 function issuu_painel_textdomain()
@@ -23,9 +30,12 @@ function issuu_painel_textdomain()
 
 add_option(ISSUU_PAINEL_PREFIX . 'api_key', '');
 add_option(ISSUU_PAINEL_PREFIX . 'api_secret', '');
+add_option(ISSUU_PAINEL_PREFIX . 'enabled_user', 'Administrator');
 
 $api_key = get_option(ISSUU_PAINEL_PREFIX . 'api_key');
 $api_secret = get_option(ISSUU_PAINEL_PREFIX . 'api_secret');
+$enabled_user = get_option(ISSUU_PAINEL_PREFIX . 'enabled_user');
+$issuu_painel_capacity = $issuu_painel_capabilities[$enabled_user];
 
 require(ISSUU_PAINEL_DIR . 'menu/principal/config.php');
 require(ISSUU_PAINEL_DIR . 'issuuservice/issuu-lib.php');
@@ -48,7 +58,14 @@ function issuu_painel_wp_enqueue_scripts()
 	wp_enqueue_style('issuu-painel-documents', ISSUU_PAINEL_URL . 'css/issuu-painel-documents.css');
 	wp_enqueue_script(
 		'issuu-iframe-link',
-		ISSUU_PAINEL_URL . 'js/issuu-iframe-link.js',
+		ISSUU_PAINEL_URL . 'js/issuu-iframe-link.min.js',
+		array('jquery'),
+		null,
+		true
+	);
+	wp_enqueue_script(
+		'issuu-replace-tags',
+		ISSUU_PAINEL_URL . 'js/replace-tags-p.min.js',
 		array('jquery'),
 		null,
 		true
@@ -110,18 +127,29 @@ add_action('admin_menu', 'ip_menu_admin');
 
 function ip_menu_admin()
 {
-	global $api_key, $api_secret;
-
-	do_action(ISSUU_PAINEL_PREFIX . 'menu_page');
+	global $api_key, $api_secret, $issuu_painel_capabilities, $issuu_painel_capacity;
 
 	if ($_SERVER['REQUEST_METHOD'] == 'POST' && (isset($_GET['page']) && $_GET['page'] == 'issuu-painel-admin'))
 	{
 		update_option(ISSUU_PAINEL_PREFIX . 'api_key', trim($_POST['api_key']));
-		update_option(ISSUU_PAINEL_PREFIX . 'api_secret',trim($_POST['api_secret']));
+		update_option(ISSUU_PAINEL_PREFIX . 'api_secret', trim($_POST['api_secret']));
+
+		if (in_array($_POST['enabled_user'], array('Administrator', 'Editor', 'Author')))
+		{
+			update_option(ISSUU_PAINEL_PREFIX . 'enabled_user', $_POST['enabled_user']);
+		}
+		else
+		{
+			$_POST['enabled_user'] = 'Administrator';
+			update_option(ISSUU_PAINEL_PREFIX . 'enabled_user', 'Administrator');
+		}
 
 		$api_key = trim($_POST['api_key']);
 		$api_secret = trim($_POST['api_secret']);
+		$issuu_painel_capacity = $issuu_painel_capabilities[$_POST['enabled_user']];
 	}
+
+	do_action(ISSUU_PAINEL_PREFIX . 'menu_page');
 
 	if ((!is_null($api_key) && strlen($api_key) > 0) && (!is_null($api_secret) && strlen($api_secret) > 0))
 	{
