5ea806cce17c3085d3c443bb0c9dc794f17ba24d
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2015-07-02 23:10:55 -0300

    Fechando Reader

--- includes/reader/js/jquery.issuupanelreader.js
+++ includes/reader/js/jquery.issuupanelreader.js
@@ -1,18 +1,47 @@
+/*
+*	jQuery Issuu Panel Simple Reader
+*
+*
+*	Author: Pedro Marcelo
+*	Version: 1.0
+*/
+
 var $nav, $ul, $prev, $next, $iframe, $tools;
+var keyMap = {
+	16 : false,
+	17 : false,
+	37 : false,
+	38 : false,
+	39 : false,
+	40 : false
+};
 
 (function($){
 	$.fn.issuuPanelReader = function(options){
 		var defaults = {
 			prevNavigation : "#ip-reader-prev",
 			nextNavigation : "#ip-reader-next",
+			zoomMax : '.ip-zoom-max',
 			zoomMore : '.ip-zoom-more',
 			zoomMinus : '.ip-zoom-minus',
+			zoomMin : '.ip-zoom-min',
 			closeReader : '.ip-close-reader'
 		};
 		$.issuuPanelReaderIframe = $(this).contents();
 		$.issuuPanelReader = {
 			zoom : 100
 		};
+
+		$(this).on('mousedown', function(e) {
+			$(this).on('mousemove', function(evt) {
+				$.issuuPanelReaderIframe.find('html,body').stop(false, true).animate({
+					scrollLeft: e.pageX - evt.clientX
+				});
+			});
+		});
+		$(this).on('mouseup', function() {
+			$(this).off('mousemove');
+		});
 
 		options = $.extend({}, defaults, options);
 
@@ -46,6 +75,16 @@ var $nav, $ul, $prev, $next, $iframe, $tools;
 				$.issuuPanelReaderIframe
 					.find('[data-ip-reader-page="' + pageNumber + '"]').parent().addClass('ip-doc-active');
 				$.issuuPanelReaderIframe.find('body').css({zoom : $.issuuPanelReader.zoom + '%'});
+			}
+		});
+
+		$(options.zoomMax).click(function(e){
+			e.preventDefault();
+
+			if ($.issuuPanelReader.zoom < 500)
+			{
+				$.issuuPanelReader.zoom = 500;
+				$.issuuPanelReaderIframe.find('#issuu-panel-document').css({zoom : $.issuuPanelReader.zoom + '%'});
 			}
 		});
 
@@ -85,73 +124,61 @@ var $nav, $ul, $prev, $next, $iframe, $tools;
 			$.issuuPanelReaderIframe.find('#issuu-panel-document').css({zoom : $.issuuPanelReader.zoom + '%'});
 		});
 
+		$(options.zoomMin).click(function(e){
+			e.preventDefault();
+
+			if ($.issuuPanelReader.zoom > 100)
+			{
+				$.issuuPanelReader.zoom = 100;
+				$.issuuPanelReaderIframe.find('#issuu-panel-document').css({zoom : $.issuuPanelReader.zoom + '%'});
+			}
+		});
+
 		$(options.closeReader).click(function(e){
 			$('#issuu-panel-reader').fadeOut(500, function(){
 				$(this).remove();
 				$iframe = null;
+				$(window).unbind('keyup').unbind('keydown');
+				$('body').removeClass('issuu-panel-noscroll');
 			});
+		});
+
+		$(document).keydown(function(e){
+			switch(e.keyCode) {
+				case 27:
+					$(options.closeReader).trigger('click');
+					break;
+				default:
+					if (e.keyCode in keyMap) {
+						keyMap[e.keyCode] = true;
+						if (keyMap[17] && keyMap[37]) {
+							$(options.prevNavigation).trigger('click');
+							return false;
+						} else if (keyMap[17] && keyMap[16] && keyMap[38]) {
+							$(options.zoomMax).trigger('click');
+							return false;
+						} else if (keyMap[17] && keyMap[38]) {
+							$(options.zoomMore).trigger('click');
+							return false;
+						} else if (keyMap[17] && keyMap[39]) {
+							$(options.nextNavigation).trigger('click');
+							return false;
+						} else if (keyMap[17] && keyMap[16] && keyMap[40]) {
+							$(options.zoomMin).trigger('click');
+							return false;
+						} else if (keyMap[17] && keyMap[40]) {
+							$(options.zoomMinus).trigger('click');
+							return false;
+						}
+					}
+					break;
+			}
+		}).keyup(function(e){
+		    if (e.keyCode in keyMap) {
+		        keyMap[e.keyCode] = false;
+		    }
 		});
 
 		return this;
 	}
-
-	$(document).ready(function(){
-		$('[data-toggle="issuu-panel-reader"]').click(function(e){
-			e.preventDefault();
-			$('<div>',{
-				id : 'issuu-panel-reader'
-			}).appendTo('body');
-			$('<div>', {
-				id: 'botao-issuu'
-			}).appendTo('#issuu-panel-reader');
-			$(document).keyup(function(e){
-				if (e.keyCode == 27)
-				{
-					$('#issuu-panel-reader').fadeOut(500, function(){
-						$(this).remove();
-						$iframe = null;
-					});
-				}
-			});
-			$('#issuu-panel-reader').hide().fadeIn(500);
-			$('#botao-issuu').delay(2000).fadeOut(500, function(){
-				$(this).remove();
-				$('<iframe>', {
-					id : 'ip-iframe-reader',
-					src : 'reader.html'
-				})
-				.appendTo('#issuu-panel-reader')
-				.load(function(){
-					$(this).issuuPanelReader();
-					$iframe = $(this).contents();
-				});
-			});
-			$tools = $('<div>', {
-				id : 'issuu-panel-reader-tools'
-			}).appendTo('#issuu-panel-reader');
-			$('<div>', {
-				'class' : 'issuu-panel-reader-tools ip-zoom-more'
-			}).appendTo($tools);
-			$('<div>', {
-				'class' : 'issuu-panel-reader-tools ip-zoom-minus'
-			}).appendTo($tools);
-			$('<div>', {
-				'class' : 'issuu-panel-reader-tools ip-close-reader'
-			}).appendTo($tools);
-			$nav = $('<nav>').appendTo('#issuu-panel-reader');
-			$ul = $('<ul>').appendTo($nav);
-			$prev = $('<li>').appendTo($ul);
-			$next = $('<li>').appendTo($ul);
-			$('<a>', {
-				href: '#',
-				id : 'ip-reader-prev',
-				'class' : 'ip-reader-navigation'
-			}).appendTo($prev);
-			$('<a>', {
-				href: '#',
-				id : 'ip-reader-next',
-				'class' : 'ip-reader-navigation'
-			}).appendTo($next);
-		});
-	});
 })(jQuery);
