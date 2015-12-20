63bafbe1598e81b5ee321f79cdf10572b4761df8
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2015-12-20 12:23:25 -0200

    Classe gerenciadora do cache do shortcode

---
+++ includes/classes/class.issuupanelcachemanager.php
@@ -0,0 +1,66 @@
+<?php
+
+class IssuuPanelCacheManager
+{
+	private $issuuPanelOptionEntity;
+
+	private $issuuPanelShortcodeCache;
+
+	public function __construct($issuuPanelOptionEntity)
+	{
+		$this->setOptionEntity($issuuPanelOptionEntity);
+		$this->issuuPanelShortcodeCache = $this->getOptionEntity()->getCache();
+	}
+
+	public function cacheIsActive()
+    {
+        return ($this->getOptionEntity()->getCacheStatus() == 'active');
+    }
+
+	public function setCache($shortcode, $content = '', $params = array(), $page = 1)
+    {
+        $key = $this->generateShortcodeKey($shortcode, $params);
+
+        if (!isset($this->issuuPanelShortcodeCache[$key]))
+        {
+            $this->issuuPanelShortcodeCache[$key] = array();
+        }
+
+        $this->issuuPanelShortcodeCache[$key][$page] = $content;
+    }
+
+    public function getCache($shortcode, $params = array(), $page = 1)
+    {
+        $key = $this->generateShortcodeKey($shortcode, $params);
+
+        if (isset($this->issuuPanelShortcodeCache[$key]))
+        {
+            if (isset($this->issuuPanelShortcodeCache[$key][$page]))
+            {
+                return $this->issuuPanelShortcodeCache[$key][$page];
+            }
+        }
+
+        return '';
+    }
+
+    public function updateCache($shortcode = null, $content = '', $params = array(), $page = 1)
+    {
+        if (!is_null($shortcode))
+        {
+            $this->setCache($shortcode, $content, $params, $page);
+        }
+        $this->getOptionEntity()->setShortcodeCache($this->issuuPanelShortcodeCache);
+        // update_option(ISSUU_PANEL_PREFIX . 'shortcode_cache', $this->serializeCache());
+    }
+
+	private function setOptionEntity(IssuuPanelOptionEntity $issuuPanelOptionEntity)
+	{
+		$this->issuuPanelOptionEntity = $issuuPanelOptionEntity;
+	}
+
+	public function getOptionEntity()
+	{
+		return $this->issuuPanelOptionEntity;
+	}
+}
