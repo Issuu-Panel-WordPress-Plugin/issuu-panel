e0bc341577d9d84a3bc054aecb52a64d2764fc11
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2016-01-02 18:47:44 -0200

    Reformulando código do menu de documentos

--- menu/document/config.php
+++ menu/document/config.php
@@ -13,177 +13,63 @@ class IssuuPanelPageDocuments extends IssuuPanelSubmenu
 	public function page()
 	{
 		$this->getConfig()->getIssuuPanelDebug()->appendMessage("Issuu Panel Page (Documents)");
-		$action = new IssuuPanelAction();
-
-		echo '<div class="wrap">';
-
 		$issuuDocument = $this->getConfig()->getIssuuServiceApi('IssuuDocument');
+		$issuuFolder = $this->getConfig()->getIssuuServiceApi('IssuuFolder');
+		$subpage = filter_input(INPUT_GET, 'issuu-panel-subpage');
+		$validPages = array('upload', 'url-upload', 'update');
+		try {
+			switch ($subpage) {
+				case 'upload':
+					$folders = $issuuFolder->issuuList();
+					$cnt_f = (isset($folders['folder']))? count($folders['folder']) : 0;
+				case 'url-upload':
+					include(ISSUU_PANEL_DIR . "menu/document/forms/$subpage.php");
+					break;
+				case 'update':
+					$doc = $issuuDocument->update(array(
+						'name' => filter_input(INPUT_GET, 'document')
+					));
+					$tags = '';
 
-		$action->setParam('issuuDocument', $issuuDocument);
+					if ($doc['stat'] == 'ok' && !empty($doc['document']))
+					{
+						$doc = $doc['document'];
 
-		if (isset($_GET['upload']) && !isset($_POST['delete']))
-		{
-			if ($_SERVER['REQUEST_METHOD'] == 'POST')
-			{
-				$action = $this->getConfig()->getHookManager()->triggerAction(
-					'issuu-panel-document-upload',
-					null,
-					array(
-						'postData' => filter_input_array(INPUT_POST),
-						'config' => $this->getConfig()
-					)
-				);
+						if (isset($doc->tags))
+						{
+							$tags = implode(',', $doc->tags);
+						}
+					}
+					else
+					{
+						$this->getErrorMessage(get_issuu_message('No documents found'));
+						return;
+					}
+					include(ISSUU_PANEL_DIR . 'menu/document/forms/update.php');
+					break;
+				case null:
+					$image = 'http://image.issuu.com/%s/jpg/page_1_thumb_large.jpg';
+					$page = (intval(filter_input(INPUT_GET, 'pn')))? : 1;
+					$per_page = 10;
+					$docs = $issuuDocument->issuuList(array(
+						'pageSize' => $per_page,
+						'startIndex' => $per_page * ($page - 1)
+					));
+					
+					if (isset($docs['totalCount']) && $docs['totalCount'] > $docs['pageSize'])
+					{
+						$number_pages = ceil($docs['totalCount'] / $per_page);
+					}
+					require(ISSUU_PANEL_DIR . 'menu/document/document-list.php');
+					break;
+				default:
+					$this->getConfig()->getIssuuPanelDebug()->appendMessage("Page not found");
+					$this->getErrorMessage(get_issuu_message('This page not exists'));
+					return;
 			}
-			else
-			{
-				try {
-					$issuu_folder = $this->getConfig()->getIssuuServiceApi('IssuuFolder');
-					$folders = $issuu_folder->issuuList();
-				} catch (Exception $e) {
-					$this->getConfig()->getIssuuPanelDebug()->appendMessage("Page Exception - " . $e->getMessage());
-					return "";
-				}
-
-				$cnt_f = (isset($folders['folder']))? count($folders['folder']) : 0;
-				include(ISSUU_PANEL_DIR . 'menu/document/forms/upload.php');
-				$load = true;
-			}
-		}
-		else if (isset($_GET['url_upload']) && !isset($_POST['delete']))
-		{
-			if ($_SERVER['REQUEST_METHOD'] == 'POST')
-			{
-				$action = $this->getConfig()->getHookManager()->triggerAction(
-					'issuu-panel-document-url-upload',
-					null,
-					array(
-						'issuuDocument' => $issuuDocument
-					)
-				);
-			}
-			else
-			{
-				try {
-					$issuu_folder = $this->getConfig()->getIssuuServiceApi('IssuuFolder');
-					$folders = $issuu_folder->issuuList();
-				} catch (Exception $e) {
-					$this->getConfig()->getIssuuPanelDebug()->appendMessage("Page Exception - " . $e->getMessage());
-					return "";
-				}
-
-				$cnt_f = (isset($folders['folder']))? count($folders['folder']) : 0;
-				include(ISSUU_PANEL_DIR . 'menu/document/forms/url-upload.php');
-				$load = true;
-			}
-		}
-		else if (isset($_GET['ip-update']) && strlen($_GET['ip-update']) > 0)
-		{
-			$load = true;
-
-			if ($_SERVER['REQUEST_METHOD'] == 'POST')
-			{
-				$action = $this->getConfig()->getHookManager()->triggerAction(
-					'issuu-panel-document-update',
-					null,
-					array(
-						'issuuDocument' => $issuuDocument
-					)
-				);
-
-				$doc = $action->getParam('result');
-				$load = false;
-			}
-			else
-			{
-				$params['name'] = strtr($_GET['ip-update'], array('%20' => '+', ' ' => '+'));
-
-				try {
-					$doc = $issuuDocument->update($params);
-				} catch (Exception $e) {
-					$this->getConfig()->getIssuuPanelDebug()->appendMessage("Page Exception - " . $e->getMessage());
-					return "";
-				}
-			}
-
-			if ($doc['stat'] == 'ok' && !empty($doc['document']))
-			{
-				if ($load)
-				{
-					$doc = $doc['document'];
-				}
-				else
-				{
-					$doc = $doc['document'];
-				}
-			}
-			else
-			{
-				echo '<div class="error"><p>' . get_issuu_message('No documents found') . '</p></div>';
-				exit;
-			}
-
-			$tags = '';
-
-			if (isset($doc->tags))
-			{
-				foreach ($doc->tags as $tag) {
-					$tags .= $tag . ',';
-				}
-			}
-
-			if (($length = strlen($tags)) > 0)
-			{
-				$tags = substr($tags, 0, $length - 1);
-			}
-
-			include(ISSUU_PANEL_DIR . 'menu/document/forms/update.php');
-		}
-
-		echo $action->getParam('message', '');
-
-		if (!isset($load))
-		{
-			if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'POST' && filter_input(INPUT_POST, 'delete') == 'true')
-			{
-				try {
-					$action = $this->getConfig()->getHookManager()->triggerAction(
-						'issuu-panel-document-delete',
-						null,
-						array(
-							'issuuDocument' => $issuuDocument,
-							'postData' => filter_input_array(INPUT_POST),
-						)
-					);
-					require(ISSUU_PANEL_DIR . 'menu/document/requests/delete.php');
-				} catch (Exception $e) {
-					$this->getConfig()->getIssuuPanelDebug()->appendMessage(
-						"Document Delete Exception - " . $e->getMessage()
-					);
-					return "";
-				}
-			}
-
-			$image = 'http://image.issuu.com/%s/jpg/page_1_thumb_large.jpg';
-			$page = (isset($_GET['pn']))? $_GET['pn'] : 1;
-			$per_page = 10;
-			$params = array(
-				'pageSize' => $per_page,
-				'startIndex' => $per_page * ($page - 1)
-			);
-
-			try {
-				$docs = $issuuDocument->issuuList($params);
-			} catch (Exception $e) {
-				$this->getConfig()->getIssuuPanelDebug()->appendMessage("Page Exception - " . $e->getMessage());
-				return "";
-			}
-			
-			if (isset($docs['totalCount']) && $docs['totalCount'] > $docs['pageSize'])
-			{
-				$number_pages = ceil($docs['totalCount'] / $per_page);
-			}
-
-			require(ISSUU_PANEL_DIR . 'menu/document/document-list.php');
+		} catch (Exception $e) {
+			$this->getConfig()->getIssuuPanelDebug()->appendMessage("Page Exception - " . $e->getMessage());
+			$this->getErrorMessage(get_issuu_message('An error occurred while we try connect to Issuu'));
 		}
 	}
 }
