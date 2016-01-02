55e14a9db79bc12b76229ca266f4e36e6a7ac747
pedromarcelojava <pedromarcelodesaalves@gmail.com>
2016-01-02 18:55:05 -0200

    Adicionando página dos documentos dentro do wrap

--- menu/document/document-list.php
+++ menu/document/document-list.php
@@ -1,57 +1,59 @@
-<h1><?php the_issuu_message('Documents list'); ?></h1>
-<div id="issuu-panel-ajax-result">
-	<p></p>
-</div>
-<form action="" method="post" id="delete-documents">
-	<input type="hidden" name="delete" value="true">
-	<a href="admin.php?page=issuu-document-admin&upload" class="buttons-top button-primary" title="">
-		<?php the_issuu_message('Upload file'); ?>
-	</a>
-	<a href="admin.php?page=issuu-document-admin&url_upload" class="buttons-top button-primary" title="">
-		<?php the_issuu_message('Upload file by URL'); ?>
-	</a>
-	<input type="submit" class="buttons-top button-secondary button-danger" value="<?php the_issuu_message('Delete'); ?>">
-	<?php if (isset($docs['totalCount']) && $docs['totalCount'] > $docs['pageSize']) : ?>
-		<div id="issuu-painel-pagination">
-			<?php for ($i = 1; $i <= $number_pages; $i++) : ?>
-				<?php if ($page == $i) : ?>
-					<span class="issuu-painel-number-page"><?= $i; ?></span>
-				<?php else : ?>
-					<a class="issuu-painel-number-page" href="?page=issuu-document-admin&pn=<?= $i; ?>"><?= $i; ?></a>
-				<?php endif; ?>
-			<?php endfor; ?>
-		</div>
-	<?php endif; ?>
-	<div id="document-list">
-		<?php if (isset($docs['document']) && !empty($docs['document'])) : ?>
-			<?php foreach ($docs['document'] as $doc) : ?>
-				<?php if (empty($doc->coverWidth) && empty($doc->coverHeight)) : ?>
-					<div id="<?= $doc->orgDocName; ?>" class="document converting">
-						<input type="checkbox" name="name[]" class="issuu-checkbox" value="<?= $doc->name; ?>">
-						<div class="document-box">
-							<div class="loading-issuu"></div>
-				<?php else: ?>
-					<div class="document complete">
-						<input type="checkbox" name="name[]" class="issuu-checkbox" value="<?= $doc->name; ?>">
-						<div class="document-box">
-							<img src="<?= sprintf($image, $doc->documentId) ?>" alt="">
-							<div class="update-document">
-								<a href="admin.php?page=issuu-document-admin&ip-update=<?= $doc->name; ?>">
-									<?php the_issuu_message('Edit'); ?>
-								</a>
-							</div>
-				<?php endif; ?>
-					</div>
-					<p class="description"><?= $doc->title ?></p>
-				</div>
-			<?php endforeach; ?>
-		<?php endif; ?>
+<div class="wrap">
+	<h1><?php the_issuu_message('Documents list'); ?></h1>
+	<div id="issuu-panel-ajax-result">
+		<p></p>
 	</div>
-</form>
-<?php if (isset($docs['document']) && !empty($docs['document'])) : ?>
-	<h3>Shortcode</h3>
-	<input type="text" value="[issuu-painel-document-list]" disabled size="28" class="code shortcode">
-<?php endif; ?>
+	<form action="" method="post" id="delete-documents">
+		<input type="hidden" name="delete" value="true">
+		<a href="admin.php?page=issuu-document-admin&upload" class="buttons-top button-primary" title="">
+			<?php the_issuu_message('Upload file'); ?>
+		</a>
+		<a href="admin.php?page=issuu-document-admin&url_upload" class="buttons-top button-primary" title="">
+			<?php the_issuu_message('Upload file by URL'); ?>
+		</a>
+		<input type="submit" class="buttons-top button-secondary button-danger" value="<?php the_issuu_message('Delete'); ?>">
+		<?php if (isset($docs['totalCount']) && $docs['totalCount'] > $docs['pageSize']) : ?>
+			<div id="issuu-painel-pagination">
+				<?php for ($i = 1; $i <= $number_pages; $i++) : ?>
+					<?php if ($page == $i) : ?>
+						<span class="issuu-painel-number-page"><?= $i; ?></span>
+					<?php else : ?>
+						<a class="issuu-painel-number-page" href="?page=issuu-document-admin&pn=<?= $i; ?>"><?= $i; ?></a>
+					<?php endif; ?>
+				<?php endfor; ?>
+			</div>
+		<?php endif; ?>
+		<div id="document-list">
+			<?php if (isset($docs['document']) && !empty($docs['document'])) : ?>
+				<?php foreach ($docs['document'] as $doc) : ?>
+					<?php if (empty($doc->coverWidth) && empty($doc->coverHeight)) : ?>
+						<div id="<?= $doc->orgDocName; ?>" class="document converting">
+							<input type="checkbox" name="name[]" class="issuu-checkbox" value="<?= $doc->name; ?>">
+							<div class="document-box">
+								<div class="loading-issuu"></div>
+					<?php else: ?>
+						<div class="document complete">
+							<input type="checkbox" name="name[]" class="issuu-checkbox" value="<?= $doc->name; ?>">
+							<div class="document-box">
+								<img src="<?= sprintf($image, $doc->documentId) ?>" alt="">
+								<div class="update-document">
+									<a href="admin.php?page=issuu-document-admin&ip-update=<?= $doc->name; ?>">
+										<?php the_issuu_message('Edit'); ?>
+									</a>
+								</div>
+					<?php endif; ?>
+						</div>
+						<p class="description"><?= $doc->title ?></p>
+					</div>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</div>
+	</form>
+	<?php if (isset($docs['document']) && !empty($docs['document'])) : ?>
+		<h3>Shortcode</h3>
+		<input type="text" value="[issuu-painel-document-list]" disabled size="28" class="code shortcode">
+	<?php endif; ?>
+</div>
 <script type="text/javascript">
 	(function($){
 		$(document).ready(function(){
